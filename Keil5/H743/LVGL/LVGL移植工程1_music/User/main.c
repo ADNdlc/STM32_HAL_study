84450f21b238705d0b7c2f947cc3ebd2b3e21d26
ADMddlc <1211428948@qq.com>
2025-06-28 15:40:49 +0800

    目录结构

---
+++ Keil5/H743/LVGL/LVGL移植工程1_music/User/main.c
@@ -0,0 +1,74 @@
+/**
+ ****************************************************************************************************
+ * @file        main.c
+ * @author      正点原子团队(ALIENTEK)
+ * @version     V1.0
+ * @date        2022-09-06
+ * @brief       触摸屏 实验
+ * @license     Copyright (c) 2020-2032, 广州市星翼电子科技有限公司
+ ****************************************************************************************************
+ * @attention
+ *
+ * 实验平台:正点原子 阿波罗 H743开发板
+ * 在线视频:www.yuanzige.com
+ * 技术论坛:www.openedv.com
+ * 公司网址:www.alientek.com
+ * 购买地址:openedv.taobao.com
+ *
+ ****************************************************************************************************
+ */
+
+#include "./SYSTEM/sys/sys.h"
+#include "./SYSTEM/usart/usart.h"
+#include "./SYSTEM/delay/delay.h"
+#include "./BSP/SDRAM/sdram.h"
+#include "./BSP/LED/led.h"
+#include "./BSP/LCD/lcd.h"
+#include "./BSP/KEY/key.h"
+#include "./BSP/MPU/mpu.h"
+#include "./BSP/TOUCH/touch.h"
+
+/* LVGL相关包含 */
+#include "./BSP/TIMER/btim.h"
+#include "lvgl.h"
+#include "lv_port_disp_template.h"
+#include "lv_port_indev_template.h"
+//#include "lv_demo_stress.h"
+#include "lv_demo_music.h"
+
+int main(void)
+{
+    
+    
+    sys_cache_enable();                        /* 打开L1-Cache */
+    HAL_Init();                                /* 初始化HAL库 */
+    sys_stm32_clock_init(160, 5, 2, 4);        /* 设置时钟, 400Mhz */
+    delay_init(400);                           /* 延时初始化 */
+    usart_init(115200);                        /* 串口初始化 */
+    mpu_memory_protection();                   /* 保护相关存储区域 */
+    led_init();                                /* 初始化LED */
+	key_init();                                /* 初始化KEY */
+    sdram_init();                              /* 初始化SDRAM */
+    
+	/* LVGL相关初始化 */
+	btim_timx_int_init(100-1, 2000-1);	//定时器(时基) 时钟200Mhz	psc=2000, arr=100
+	lv_init();
+	lv_port_disp_init();
+	lv_port_indev_init();
+	
+//	lv_obj_t* switch_obj = lv_switch_create(lv_scr_act());
+//	lv_obj_set_size(switch_obj, 120, 60);
+//	lv_obj_align(switch_obj, LV_ALIGN_CENTER, 0, 0);
+	
+//	lv_demo_stress();	//压力测试
+	
+	lv_demo_music();	//音乐播放器
+	while(1){
+		delay_ms(5);
+		lv_timer_handler();
+		
+		
+	}
+}
+
+
